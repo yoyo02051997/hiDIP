067c2758fe78819975c7e1318ea82d754db6c244
yoyo02051997 <yoyo02051997@gmail.com>
2022-08-30 20:40:24 +0800

    Add files via upload

--- R/Subfunction.R
+++ R/Subfunction.R
@@ -169,21 +169,6 @@ entropy_true <- function(p, q) {
   sapply(q, sub)
 }
 
-cppFunction(
-  "NumericVector qD_MLE(NumericVector q,NumericVector ai){
-  const int length = q.size();
-  const int S = ai.size();
-  NumericVector Q(length);
-  NumericVector temp(S);
-  for(int j = 0; j<length;j++){
-  for(int i = 0 ; i<S;i++){
-  temp[i] = pow(ai[i],q[j]);
-  }
-  Q[j] = (1-sum(temp)) / (q[j]-1);
-  }
-  return Q;
-  }")
-
 entropy_true_inc <- function(p, q){
   x = p[-1]
   U = sum(x)
@@ -226,31 +211,6 @@ entropy_est <- function(x, q){
   }
   sapply(q, Sub)
 }
-
-cppFunction(
-  "NumericVector qDSub2(double q, int f1, double A, const int n){
-  NumericVector tmp(n);
-  double out = 0;
-  for(int r=0;r<=(n-1);r++){
-  tmp[r] = Rf_choose(q-1, r)*pow(A-1, r);
-  }
-  return tmp;
-  }")
-
-cppFunction(
-  "NumericVector qDSub(double q, NumericVector Xi,const int n){
-  const int len = Xi.size();
-  NumericVector Out(len);
-  for(int k=0;k<=(len-1);k++){
-  int z = Xi[k];
-  NumericVector tmp(n-z+1);
-  for(int l=0;l<=(n-z);l++){
-  tmp[l] = Rf_choose(l-q,l)*exp(Rf_lchoose(n-l-1, z-1)-Rf_lchoose(n, z));
-  }
-  Out[k] = sum(tmp);
-  }
-  return Out;
-  }")
 
 max_est <- function(x, q){
   x = x[x > 0]
---
+++ src/cpp.cpp
@@ -0,0 +1,47 @@
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector qD_MLE(NumericVector q,NumericVector ai){
+  const int length = q.size();
+  const int S = ai.size();
+  NumericVector Q(length);
+  NumericVector temp(S);
+  for(int j = 0; j<length;j++){
+    for(int i = 0 ; i<S;i++){
+      temp[i] = pow(ai[i],q[j]);
+    }
+    Q[j] = (1-sum(temp)) / (q[j]-1);
+  }
+  return Q;
+}
+
+// [[Rcpp::export]]
+NumericVector qDSub2(double q, int f1, double A, const int n){
+  NumericVector tmp(n);
+  double out = 0;
+  for(int r=0;r<=(n-1);r++){
+    tmp[r] = Rf_choose(q-1, r)*pow(A-1, r);
+  }
+  return tmp;
+}
+
+// [[Rcpp::export]]
+NumericVector qDSub(double q, NumericVector Xi,const int n){
+  const int len = Xi.size();
+  NumericVector Out(len);
+  for(int k=0;k<=(len-1);k++){
+    int z = Xi[k];
+    NumericVector tmp(n-z+1);
+    for(int l=0;l<=(n-z);l++){
+      tmp[l] = Rf_choose(l-q,l)*exp(Rf_lchoose(n-l-1, z-1)-Rf_lchoose(n, z));
+    }
+    Out[k] = sum(tmp);
+  }
+  return Out;
+}
+
+
+
+
